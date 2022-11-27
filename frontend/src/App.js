b3dfb84224559887a0cc17f745511495243966f5
Bill Tran <tung.tran@tcu.edu>
2022-11-27 14:07:49 -0600

    unsplash api

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import Header from './components/Header'
 import Search from './components/Search'
 import { useState } from 'react';
 
+const UNSPLASH_KEY = "YrA_xr58dOu5W_SKJ_kB5xqY1hVNX8Q1oxgqQ6rFaCk";
 
 const App = () => {
   const [word, setWord] = useState('');
@@ -11,6 +12,14 @@ const App = () => {
   const handleSearchSubmit = (e) => {
   e.preventDefault();
   console.log(word)
+  fetch(`https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data)
+    })
+    .catch((err) => {
+      console.log(err);
+    })
   }
 
   return (
