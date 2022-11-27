5b6c6ed94ae1f4383311d2a5cb1035f9529ef679
Bill Tran <tung.tran@tcu.edu>
2022-11-27 17:56:04 -0600

    display and delete

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,34 +1,36 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import React from 'react';
-import Header from './components/Header'
-import Search from './components/Search'
-import { useState } from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+import React from "react";
+import Header from "./components/Header";
+import Search from "./components/Search";
+import { useState } from "react";
 
 const UNSPLASH_KEY = "YrA_xr58dOu5W_SKJ_kB5xqY1hVNX8Q1oxgqQ6rFaCk";
 
 const App = () => {
-  const [word, setWord] = useState('');
+  const [word, setWord] = useState("");
 
   const handleSearchSubmit = (e) => {
-  e.preventDefault();
-  console.log(word)
-  fetch(`https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`)
-    .then((res) => res.json())
-    .then((data) => {
-      console.log(data)
-    })
-    .catch((err) => {
-      console.log(err);
-    })
-    setWord("")
-  }
+    e.preventDefault();
+    console.log(word);
+    fetch(
+      `https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    setWord("");
+  };
 
   return (
     <div>
-      <Header title='Images Gallery'/>
-      <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit}/>
+      <Header title="Images Gallery" />
+      <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit} />
     </div>
   );
-}
+};
 
 export default App;
