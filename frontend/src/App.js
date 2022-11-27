3a8333fd7ec73041bca9835afe7b713f00ffeabd
Bill Tran <tung.tran@tcu.edu>
2022-11-27 17:56:24 -0600

    push

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,11 +3,14 @@ import React from "react";
 import Header from "./components/Header";
 import Search from "./components/Search";
 import { useState } from "react";
+import ImageCard from "./components/ImageCard";
+import {Container, Row, Col} from "react-bootstrap";
 
 const UNSPLASH_KEY = "YrA_xr58dOu5W_SKJ_kB5xqY1hVNX8Q1oxgqQ6rFaCk";
 
 const App = () => {
   const [word, setWord] = useState("");
+  const [images, setImages] = useState([]);
 
   const handleSearchSubmit = (e) => {
     e.preventDefault();
@@ -17,7 +20,7 @@ const App = () => {
     )
       .then((res) => res.json())
       .then((data) => {
-        console.log(data);
+        setImages([{...data, title:word}, ...images])
       })
       .catch((err) => {
         console.log(err);
@@ -25,10 +28,23 @@ const App = () => {
     setWord("");
   };
 
+  const handleDeleteImage = (id) => {
+    setImages(images.filter((image) => image.id !== id));
+  }
+
   return (
     <div>
       <Header title="Images Gallery" />
       <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit} />
+      <Container className="mt-4">
+        <Row xs={1} md={2} lg={3}>
+          {images.map((image, i) => (
+          <Col key={i} className="pb-3">
+             <ImageCard image={image} deleteImage={handleDeleteImage}/>
+          </Col>
+          ))}
+        </Row>
+      </Container>
     </div>
   );
 };
